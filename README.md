cc7ccf3182d7e378700106b488dec647e9756ada
Matthew Beilsmith <matthewbeilsmith@gmail.com>
2019-10-10 23:15:35 -0500

    final - heavily commented script file, updated README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 Etch-A-Sketch
 
+Three sketch-pad styles!
+- CLASSIC
+- COLOR (random)
+- SHADER
+
+MODAL generation (in place of the prompt pop-up) for resizing!
+
+I worked on this for hours over a few days and learned so much!
+
 from THE ODIN PROJECT
--- js/script.js
+++ js/script.js
@@ -1,137 +1,148 @@
+// grab container to store grid generated from this file
 const gridContainer = document.getElementById('grid-container');
 
+// grab and listen for CLASSIC BRUSH click
 const classicBrush = document.getElementById('classic');
-classicBrush.addEventListener('click', () => resetBrush(classicBrush.id));
+classicBrush.addEventListener('click', () => resetBrush(classicBrush.id));  // reset sketch-pad to CLASSIC
 
+// grab and listen for COLOR BRUSH click
 const colorBrush = document.getElementById('color-bg');
-colorBrush.addEventListener('click', () =>  resetBrush(colorBrush.id));
+colorBrush.addEventListener('click', () =>  resetBrush(colorBrush.id));     // reset sketch-pad to COLOR
 
+// grab and listen for SHADER BRUSH click
 const shaderBrush = document.getElementById('shader');
-shaderBrush.addEventListener('click', () => resetBrush(shaderBrush.id));
+shaderBrush.addEventListener('click', () => resetBrush(shaderBrush.id));    // reset sketch-pad to SHADER
 
-var currentBrush = classicBrush.id;
-var opacity;
-
-var currentGridSize = 16;
+// declare & initialize global variables
+var currentBrush = classicBrush.id;                                         // set classic brush to load-default
+var currentGridSize = 16;                                                   // set 16 to be squared for load-default
+var opacity;                                                                // declare to initialice to each cell later
 
 
 
 function createGrid(size) {
 
-    size = (size > 64) ? 64 : (size < 0) ? 1 : size;
+    
+    size = (size > 64) ? 64 : (size < 0) ? 1 : size;                        // limit grid from 0 to 64^2
 
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;       // adjust #grid-container css grid columns
 
-    for (let i = 0; i < size ** 2; i++) {
-        var gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridContainer.appendChild(gridCell);
+    for (let i = 0; i < size ** 2; i++) {                                   // create cells of grid by squaring size given
+        var gridCell = document.createElement('div');                       // create cell
+        gridCell.classList.add('grid-cell');                                // style cell
+        gridContainer.appendChild(gridCell);                                // append cell to grid
     }
 
-    setBrush(currentBrush);    
+    setBrush(currentBrush);                                                 // set currently selected brush
 }
 
 
 
-function generateModal() {
 
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
+function generateModal() {                                  // generate and display modal instead of project-suggested prompt pop-up
 
-    const modalContent = document.createElement('div');
-    modalContent.classList.add('modal-content');
+    const modal = document.createElement('div');            // create full-page modal container as DIV
+    modal.classList.add('modal');                           // add style
 
-    const modalClose = document.createElement('span');
-    modalClose.classList.add('modal-close');
-    modalClose.innerHTML = "&times;";
+    const modalContent = document.createElement('div');     // create content container as DIV
+    modalContent.classList.add('modal-content');            // add style
 
-    const div = document.createElement('div');
+    const modalClose = document.createElement('span');      // create × container as SPAN
+    modalClose.classList.add('modal-close');                // add style (transitions to +)
+    modalClose.innerHTML = "&times;";                       // place × symbol in it's container
 
-    const p = document.createElement('p');
-    p.textContent = "Enter desired grid-size:";
-    p.classList.add('fl-l');
+    const div = document.createElement('div');              // create container for the next three elements as DIV
 
-    const input = document.createElement('input');
-    input.type = "text";
-    input.defaultValue = "16";
-    p.classList.add('fl-l');
+    const p = document.createElement('p');                  // create text container as P
+    p.textContent = "Enter desired grid-size:";             // add the text in P
+    p.classList.add('fl-l');                                // add float-left style
 
-    const squared = document.createElement('sup');
-    squared.textContent = "2";
-    squared.classList.add('squared');
+    const input = document.createElement('input');          // create user-input
+    input.type = "text";                                    // set input type
+    input.defaultValue = "16";                              // set default size if no input given
+    p.classList.add('fl-l');                                // add float-left style
 
-    div.innerHTML += p.outerHTML + input.outerHTML + squared.outerHTML;
-    modalContent.innerHTML += modalClose.outerHTML + div.outerHTML;
+    const squared = document.createElement('sup');          // create superscript for SQUARED visual
+    squared.textContent = "2";                              // add "2" between superscript tags
+    squared.classList.add('squared');                       // add style
 
-    document.body.insertBefore(modal, document.body.firstChild);
-    modal.appendChild(modalContent);
+    div.innerHTML += p.outerHTML + input.outerHTML + squared.outerHTML;     // assemble HTML of previous three elements into DIV
+    modalContent.innerHTML += modalClose.outerHTML + div.outerHTML;         // assemble HTML of × container and previous DIV
 
-    var newSize = modalContent.querySelector('input');
+    document.body.insertBefore(modal, document.body.firstChild);            // insert MODAL container into BODY above all else
+    modal.appendChild(modalContent);                                        // append assembled HTML to MODAL container
 
-    newSize.addEventListener('keypress', e => {
+    var newSize = modalContent.querySelector('input');                      // grab user-input
 
-        var key = e.which || e.keyCode;
-        if (key === 13) {
+    newSize.addEventListener('keypress', e => {                             // listen for pressed key within user-input
+
+        var key = e.which || e.keyCode;                                     // store pressed key
+        if (key === 13) {                                                   // if stored key is ENTER key
             
-            resize(modal, Math.round(newSize.value));
+            resize(modal, Math.round(newSize.value));                       // pass size & modal (for removal after resize)
         }
     });
     
-    modalContent.querySelector('.modal-close').onclick = () => {
+    modalContent.querySelector('.modal-close').onclick = () => {            // if × (~ +) is clicked (instead of ENTER key)
 
-        resize(modal, Math.round(newSize.value));
+        resize(modal, Math.round(newSize.value));                           // pass size & modal (for removal after resize)    
 
     }
 }
 
 
 
-function getCells () {
+
+function getCells () {                                              // grab all cells
     return document.querySelectorAll('.grid-cell');
 }
 
 
 
-function randomRgb() {
+
+function randomRgb() {                                              // generate random RGB values
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
 
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${r}, ${g}, ${b})`;                                 // return RGB for css
 }
 
 
 
-function removeGrid() {
-    getCells().forEach(cell => cell.parentNode.removeChild(cell));
+
+function removeGrid() {                                             // remove all cells from grid
+    getCells().forEach(cell => cell.parentNode.removeChild(cell));  // remove each cell from it's parent
 }
 
 
 
-function resetBrush(brush) {
 
-    currentBrush = brush;
+function resetBrush(brush) {                        // reset BRUSH from its given button-click
 
-    removeGrid();
-    createGrid(currentGridSize);
+    currentBrush = brush;                           // update passed BRUSH to current
+
+    removeGrid();                                   // remove all cells
+    createGrid(currentGridSize);                    // remake grid with last stored size and current BRUSH
 }
 
 
 
-function resetGrid() {
+
+function resetGrid() {                              // on button-click, clear drawing and reset to blank grid
 
     getCells().forEach(cell => {
 
         if (currentBrush == classicBrush.id) {
 
-            cell.classList.remove(currentBrush)
+            cell.classList.remove(currentBrush)     // remove CLASSIC BRUSH class from each cell
         }
         else {
-            cell.removeAttribute("style");
+            cell.removeAttribute("style");          // remove COLOR/SHADER BRUSH generated styling from each cell
 
             if (currentBrush == shaderBrush.id) {
 
-                cell.style.opacity = "1";
+                cell.style.opacity = "1";           // reset each cell to be opaque
             }
         }
     });
@@ -139,64 +150,66 @@ function resetGrid() {
 
 
 
-function resize(modal, size) {
 
-    currentGridSize = size;
+function resize(modal, size) {                      // pass new grid size to be created
 
-    createGrid(currentGridSize);
-    document.body.removeChild(modal);
+    currentGridSize = size;                         // update current size from user-input
+
+    createGrid(currentGridSize);                    // create grid from new size
+    document.body.removeChild(modal);               // remove MODAL
 }
 
 
 
-function resizeGrid() {
+
+function resizeGrid() {                             // on button-click
     
-    removeGrid();
-    generateModal();
+    removeGrid();                                   // remove all cells
+    generateModal();                                // create pop-up MODAL
 }
 
 
 
-function setBrush(brush) {
+function setBrush(brush) {                          // set currently selected BRUSH
 
-    if (currentBrush == shaderBrush.id)
+    if (currentBrush == shaderBrush.id)             // if SHADER
     {
         getCells().forEach(cell => {
 
-            cell.style.opacity = "1";
+            cell.style.opacity = "1";               // set each cell's opaque value
         });
     }
     
-    getCells().forEach(cell => cell.addEventListener('mouseenter', () => {
+    getCells().forEach(cell => cell.addEventListener('mouseenter', () => {      // on entering cell
 
-        if (currentBrush == classicBrush.id) {
+        if (currentBrush == classicBrush.id) {                                  // if CLASSIC 
 
-            cell.classList.add('cell-enter')
+            cell.classList.add('cell-enter');                                   // add transition style
         }
-        else if (currentBrush == colorBrush.id) {
-            cell.style.backgroundColor = randomRgb();
+        else if (currentBrush == colorBrush.id) {                               // if COLOR
+            cell.style.backgroundColor = randomRgb();                           // set background-color to a random RGB
         }
-        else {
+        else {                                                                  // if SHADER
 
-            if (cell.style.opacity <= 0) {
-                cell.style.backgroundColor = "black";
+            if (cell.style.opacity == 0) {                                      // if cell is transparent
+                cell.style.backgroundColor = "black";                           // set background-color to black
             }
             else {
-                cell.style.opacity -= 0.1;
+                cell.style.opacity -= 0.1;                                      // darken cell by 1/10th
             }
         }
     }));
     
-    if (currentBrush == classicBrush.id) {
+    if (currentBrush == classicBrush.id) {                                      // if CLASSIC
 
-        getCells().forEach(cell => cell.addEventListener('mouseleave', () => {
+        getCells().forEach(cell => cell.addEventListener('mouseleave', () => {  // on leaving cell
             
-            cell.classList.add(brush);
-            cell.classList.remove('cell-enter');
+            cell.classList.add(brush);                                          // add transparent style
+            cell.classList.remove('cell-enter');                                // remove transition style
         }));
     }
 }
 
 
 
-createGrid(currentGridSize);
+createGrid(currentGridSize);        // initialize DEFAULT grid for page load
